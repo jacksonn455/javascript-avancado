59bbad40addcd230713bf94ccbe0dffbb40e60f6
Jackson Felipe Magnabosco <jackson.magnabosco_B@pampa.compasso>
2020-02-28 16:22:43 -0300

    aula 6

--- aluraframe/client/js/app/dao/NegociacaoDao.js
+++ aluraframe/client/js/app/dao/NegociacaoDao.js
@@ -9,14 +9,13 @@ class NegociacaoDao {
     adiciona(negociacao) {
 
         return new Promise((resolve, reject) => {
-
-            let request = this
-                ._connection
-                .transaction([this._store], "readwrite")
+            
+            let request = this._connection
+                .transaction([this._store], 'readwrite')
                 .objectStore(this._store)
                 .add(negociacao);
 
-            request.onsuccess = (e) => {
+            request.onsuccess = e => {
 
                 resolve();
             };
@@ -25,40 +24,49 @@ class NegociacaoDao {
 
                 console.log(e.target.error);
                 reject('Não foi possível adicionar a negociação');
+
             };
+
         });
     }
 
     listaTodos() {
+
         return new Promise((resolve, reject) => {
 
             let cursor = this._connection
-                .transaction([this._store], "readwrite")
+                .transaction([this._store], 'readwrite')
                 .objectStore(this._store)
                 .openCursor();
+
             let negociacoes = [];
 
             cursor.onsuccess = e => {
+
                 let atual = e.target.result;
 
-                if (atual) {
-                    var dado = atual.value;
+                if(atual) {
+
+                    let dado = atual.value;
 
                     negociacoes.push(new Negociacao(dado._data, dado._quantidade, dado._valor));
 
                     atual.continue();
+
                 } else {
+
                     resolve(negociacoes);
                 }
 
             };
 
             cursor.onerror = e => {
-                console.log(e.target.error);
-                reject("Não foi possivel listar as negoaciações")
-            }
 
-        })
+                console.log(e.target.error);
+                reject('Não foi possível listar as negociações');
+            };
+
+        });
     }
 
     apagaTodos() {
@@ -75,8 +83,9 @@ class NegociacaoDao {
             request.onerror = e => {
                 console.log(e.target.error);
                 reject('Não foi possível apagar as negociações');
-            };
+            }; 
 
         });
+
     }
 }
--- aluraframe/client/js/app/services/ConnectionFactory.js
+++ aluraframe/client/js/app/services/ConnectionFactory.js
@@ -1,66 +1,76 @@
-const ConnectionFactory = (function (){  // função anonima
-const stores = ['negociacoes'];
-const version = 5;
-const dbName = 'aluraframe';
-var connection = null;
-var close = null;
 
+var ConnectionFactory = (function () {
 
-return class ConnectionFactory {
+    const stores = ['negociacoes'];
+    const version = 4;
+    const dbName = 'aluraframe';
 
-    constructor() {
+    var connection = null;
 
-        throw new Error('Não é possível criar instâncias de ConnectionFactory');
-    }
+    var close = null;
 
-    static getConnection() {
+    return class ConnectionFactory {
 
-        return new Promise((resolve, reject) => {
+        constructor() {
 
-            let openRequest = window.indexedDB.open(dbName,version);
+            throw new Error('Não é possível criar instâncias de ConnectionFactory');
+        }
 
-            openRequest.onupgradeneeded = e => {
+        static getConnection() {
+            
+            return new Promise((resolve, reject) => {
 
-                ConnectionFactory._createStores(e.target.result);
-            };
+                let openRequest = window.indexedDB.open(dbName, version);
 
-            openRequest.onsuccess = e => {
+                openRequest.onupgradeneeded = e => {
 
-                if(!connection) {
-                    connection = e.target.result;
-                    close = connection.close.bind(close);
-                    connection.close = function() {
-                          throw new Error('Você não pode fechar diretamente a conexão');
-                    };
-                }
-                resolve(connection);
+                    ConnectionFactory._createStores(e.target.result);
+
+                };
+
+                openRequest.onsuccess = e => {
+
+                    if(!connection) {
+                        connection = e.target.result;
+                        close = connection.close.bind(connection);
+                        connection.close = function() {
+                            throw new Error('Você não pode fechar diretamente a conexão');
+                        };
+                    }
+                    resolve(connection);
+
+                };
+
+                openRequest.onerror = e => {
+
+                    console.log(e.target.error);
+
+                    reject(e.target.error.name);
+                };
+
+            });
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+
+                if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true});
+
+            });
+
+        }
+
+        static closeConnection() {
+
+            if(connection) {
+                close();
+                connection = null;
+                close = null;
             }
-
-            openRequest.onerror = e => {    
-
-                console.log(e.target.error);
-
-                reject(e.target.error.name);
-            };
-        });
-    }
-    static _createStores(connection) {
-
-        stores.forEach(store => {
-
-            if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-
-    static closeConnection(){
-        if(connection) {
-            close();
-            connection = null;
         }
     }
 
-
-}
-
+    
 })();
